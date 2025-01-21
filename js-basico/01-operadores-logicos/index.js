c67ef6187f06cdbb4a689396c2e2b31fd4cbb7bd
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-21 17:58:39 -0300

    operadores lógicos e matemáticos

---
+++ js-basico/01-operadores-logicos/index.js
@@ -0,0 +1,34 @@
+let a = 3
+let b = 3
+
+// menor que ---- menor igual que
+
+a < b; 
+a <= b;
+
+// maior que ---- maior igual que
+
+a > b;
+a >= b;
+
+// igual ---- idêntico
+
+a == b;
+a === b;
+
+// negação
+
+a != b; 
+a !== b;
+
+// comparação com "E"
+
+a >= b && a === b;
+
+// comparação com "OU"
+
+a >= b || a === b;
+
+// NOT
+
+!(a === b);
---
+++ js-basico/02-operadores-matematicos/index.js
@@ -0,0 +1,22 @@
+let primeiroValor = 10
+let segundoValor = 2
+
+// Adição
+
+console.log(primeiroValor + segundoValor);
+
+// Subtração
+
+console.log(primeiroValor - segundoValor);
+
+// Multiplicação
+
+console.log(primeiroValor * segundoValor);
+
+// Divisão
+
+console.log(primeiroValor / segundoValor);
+
+// Resto
+
+console.log(primeiroValor % segundoValor);

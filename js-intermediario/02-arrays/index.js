7aa1d212c9ea286edef9dcc218afb1109cedab85
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-23 18:01:54 -0300

    arrays finalizado

--- js-intermediario/02-arrays/index.js
+++ js-intermediario/02-arrays/index.js
@@ -11,3 +11,45 @@ let filmes = [
 ]
 
 console.table(filmes)
+
+// TRABALHANDO COM ARRAY
+
+let frutas = ['banana', 'maçã', 'laranja', 'uva']
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log('fruta: ' + frutas[i])
+}
+
+// USANDO FOR EACH COM OS PARAMETROS PADROZINADOS
+
+frutas.forEach(function (item, indice, array){
+    console.log(array)
+})
+
+// PROPRIEDADES DE UM ARRAY
+
+// adicionando um elemento no FINAL do array
+frutas.push('morango') 
+console.log(frutas);
+
+
+// excluindo um elemento no FINAL do array
+frutas.pop()
+console.log(frutas);
+
+// adicionando um elemento no INICIO do array
+frutas.unshift('manga')
+console.log(frutas);
+
+// excluindo um elemento no INICIO do array
+
+frutas.shift()
+console.log(frutas);
+
+// acessando diretamente o index de um elemento
+
+frutas.indexOf()
+console.log(frutas.indexOf('laranja'));
+
+
+

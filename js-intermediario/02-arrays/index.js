c3ff451a8a08199d2c201404948b5808b036562a
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-23 02:52:48 -0300

    ajuste em functions e inicio de arrays

---
+++ js-intermediario/02-arrays/index.js
@@ -0,0 +1,13 @@
+// ARRAY NORMAL
+
+let animes = ['One piece', 'Naruto', 'Frieren']
+
+console.log(animes[0])
+
+// ARRAY MULTIDIMENSIONAL
+let filmes = [
+    ['Spider-man', 5, true, ['Tom Holland', 19]], 
+    ['Vingadores', 5, false]
+]
+
+console.table(filmes)

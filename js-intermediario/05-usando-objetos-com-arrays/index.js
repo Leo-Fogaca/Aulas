7de3cf37b9e0c9ed2cd2a746a30af96b736139d1
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-24 02:53:56 -0300

    usando objetos e arrays juntos

---
+++ js-intermediario/05-usando-objetos-com-arrays/index.js
@@ -0,0 +1,11 @@
+let videogames = {
+    nome: 'Playstation',
+    valor: 5000,
+    games: [
+        {nome: 'Spider-Man'},
+        {nome: 'It takes two'},
+        {nome: 'Fifa'}
+    ]
+}
+
+console.log(videogames.games[0].nome)

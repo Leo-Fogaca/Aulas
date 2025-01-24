32d57ee8cf07c8ad75d81e1857c1f4084f0932a2
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-24 03:28:19 -0300

    documents

---
+++ js-intermediario/06-documents/index.js
@@ -0,0 +1,23 @@
+// Usando getElementById
+
+let elementoPeloId = document.getElementById('texto1')
+
+console.log(elementoPeloId)
+
+// Usando getElementByClassName
+
+let elementoPelaClasse = document.getElementsByClassName('texto')
+
+console.log(elementoPelaClasse)
+
+// Usando getElementByTagName
+
+let elementoPelaTag = document.getElementsByTagName('h1')
+
+console.log(elementoPelaTag)
+
+// Usando querySelectorAll
+
+let usandoQuery = document.querySelectorAll("input[name='email']")
+
+console.log(usandoQuery)

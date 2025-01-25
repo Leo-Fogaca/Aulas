087546dbe56da2e2da0396d0812ba7ab52309c64
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-25 20:16:55 -0300

    pasta de exercicios JS intermediario + exercicio 1

---
+++ exercicios-js-intermediario/exercicio-1/index.js
@@ -0,0 +1,6 @@
+let botaoMudarCor = document.getElementById('mudar-cor')
+
+botaoMudarCor.addEventListener('click', function() {
+    let corAzul = document.querySelector('.azul')
+    corAzul.style.backgroundColor = 'Yellow'
+})

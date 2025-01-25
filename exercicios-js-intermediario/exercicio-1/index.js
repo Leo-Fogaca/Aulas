6677718abf092a81e6d772d83a650b76fcc865a9
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-25 20:40:45 -0300

    exercicio 2 e complemento exercicio 1

--- exercicios-js-intermediario/exercicio-1/index.js
+++ exercicios-js-intermediario/exercicio-1/index.js
@@ -3,4 +3,7 @@ let botaoMudarCor = document.getElementById('mudar-cor')
 botaoMudarCor.addEventListener('click', function() {
     let corAzul = document.querySelector('.azul')
     corAzul.style.backgroundColor = 'Yellow'
+
+    // let corAzul = document.getElementsByClassName('azul')
+    // corAzul[0].style.backgroundColor = 'Yellow'
 })
---
+++ exercicios-js-intermediario/exercicio-2/index.js
@@ -0,0 +1,9 @@
+let listaDeNumeros = [65, 44, 12, 4]
+
+listaDeNumeros.forEach((numero) => console.log(numero * 10))
+
+// for(let i = 0; i < listaDeNumeros.length; i++) {
+//     let resultado = listaDeNumeros[i] * 10
+//     console.log(resultado)
+// }
+

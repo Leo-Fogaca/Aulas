5b411c8ffb1945c3e2b050c18b8b180452feb9cb
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-01-25 22:24:09 -0300

    complemento exercicio 4

--- exercicios-js-intermediario/exercicio-4/index.js
+++ exercicios-js-intermediario/exercicio-4/index.js
@@ -9,3 +9,16 @@ inputs.forEach(input => {
         }
     })
 });
+
+// let teste = document.getElementsByClassName('input')
+
+
+// Array.from(teste).forEach(input => {
+//     input.addEventListener('change', function () {
+//         if (input.value !== "") {
+//             input.classList.add('validado')
+//         } else {
+//             input.classList.remove('validado')
+//         }
+//     })
+// })

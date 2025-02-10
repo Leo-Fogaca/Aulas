7cff31fe2a4e6907b9f8d43ff450fcfd8133facb
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-02-10 17:20:21 -0300

    setTimeout e setInterval

---
+++ js-avancado/setTimeOut-setInterval/index.js
@@ -0,0 +1,23 @@
+setTimeout(function (){
+    alert('Hello!')
+}, 4000)
+
+// encerrando um timeout
+
+// const idSetTimeout = setTimeout(function (){
+//     alert('Hello!')
+// }, 4000)
+
+// clearTimeout(idSetTimeout)
+
+setInterval(function(){
+    console.log('executou')
+}, 2000)
+
+// encerrando um interval
+
+// const idSetInterval = setInterval(function (){
+//     console.log('executou')
+// }, 2000)
+
+// clearInterval(idSetInterval)

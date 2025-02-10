6c2a73f86b298b59d9f7610f801ffed4ce84c6a2
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-02-10 15:20:56 -0300

    operador spread

---
+++ js-avancado/05-operador-spread/index.js
@@ -0,0 +1,19 @@
+//Operador spread é usado tanto para concatenar elementos iteraveis quanto para clonar elementos para poder fazer alterações sem mudar o original
+
+let pessoas1 = ['Leonardo', 'Bruna', 'Neto']
+let pessoas2 = ['Vitoria', 'Camila', 'Luiz']
+
+let concatPessoas = [ ...pessoas1, ...pessoas2]
+
+console.log(concatPessoas)
+
+//Clonando objetos
+
+let numeros = {
+    nome: 'Leonardo',
+    sobrenome: 'Fogaça'
+}
+
+let cloneNumeros = { ...numeros}
+
+console.log(cloneNumeros)

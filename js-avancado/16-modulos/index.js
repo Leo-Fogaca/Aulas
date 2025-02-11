8a50ca45f4f0c35d2a5b30329987bb9689cbd3c5
Leonardo Fogaça <leonardofogaca1998@gmail.com>
2025-02-11 15:14:14 -0300

    modulos

---
+++ js-avancado/16-modulos/index.js
@@ -0,0 +1,11 @@
+import {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir
+} from './operacoes-matematicas.js'
+
+console.log(somar(10, 10))
+console.log(subtrair(10, 10))
+console.log(multiplicar(10, 10))
+console.log(dividir(10, 10))
